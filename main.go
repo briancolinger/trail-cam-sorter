2c41ea19223903a0f37c12bdfc85c2c5a93b9642
Brian Colinger <briancolinger@users.noreply.github.com>
2023-03-17 19:43:19 -0500

    Update main.go
    Refactor code to properly handle closing of files and resources.
    Without the crashing errors this time!

--- main.go
+++ main.go
@@ -291,7 +291,16 @@ func (tcs *TrailCamSorter) processFrame(inputFile string) error {
 			frameNumber++
 			continue
 		}
-		defer frame.Close()
+
+		// Close the frame when the function completes (either normally or with an error).
+		defer func() {
+			if err := frame.Close(); err != nil {
+				log.WithFields(log.Fields{
+					"error":        err,
+					"frame_number": frameNumber,
+				}).Error("Error closing frame")
+			}
+		}()
 
 		// Create bounding boxes scaled to the width and height of the frame.
 		boundingBoxes := tcs.getBoundingBoxes(frame)
@@ -305,7 +314,6 @@ func (tcs *TrailCamSorter) processFrame(inputFile string) error {
 			frameNumber++
 			continue
 		}
-		defer joined.Close()
 
 		// Perform OCR on the joined image.
 		text, err := tcs.performOCR(joined)
@@ -339,7 +347,9 @@ func (tcs *TrailCamSorter) processFrame(inputFile string) error {
 		}).Debug("OCR: Extracted data")
 
 		// Close the joined file explicitly.
-		joined.Close()
+		if err := joined.Close(); err != nil {
+			return err
+		}
 
 		// If OCR succeeds, break out of the retry loop.
 		break
@@ -452,7 +462,11 @@ func (tcs *TrailCamSorter) readFrame(inputFile string, frameNumber int) (*gocv.M
 	if err != nil {
 		return nil, err
 	}
-	defer cap.Close()
+	defer func() {
+		if err := cap.Close(); err != nil {
+			log.WithError(err).Error("Error closing video capture")
+		}
+	}()
 
 	// Get the total number of frames in the video
 	numFrames := cap.Get(gocv.VideoCaptureFrameCount)
@@ -533,13 +547,22 @@ func (tcs *TrailCamSorter) createJoinedImage(inputFile string, frameNumber int, 
 	for _, box := range boundingBoxes {
 		// Crop out the bounding box.
 		cropped := frame.Region(box.Rect)
-		defer cropped.Close()
+		defer func() {
+			if err := cropped.Close(); err != nil {
+				log.WithField("error", err).Error("Error closing cropped image")
+			}
+		}()
 
 		// Create a blank container image to hold the label and the cropped bounding box.
 		const labelImageWidth = 800
 		const labelImageHeight = 60
 
 		labelImage = tcs.createLabelImage(box.Label, labelImageWidth, labelImageHeight)
+		defer func() {
+			if err := labelImage.Close(); err != nil {
+				log.WithField("error", err).Error("Error closing label image")
+			}
+		}()
 
 		// Overlay the cropped image onto the label image.
 		roi := labelImage.Region(image.Rectangle{Min: image.Point{0, 0}, Max: image.Point{labelImage.Cols(), labelImage.Rows()}})
@@ -573,9 +596,6 @@ func (tcs *TrailCamSorter) createJoinedImage(inputFile string, frameNumber int, 
 		}
 	}
 
-	// Close the labelImage file explicitly.
-	labelImage.Close()
-
 	if joined.Empty() {
 		return nil, fmt.Errorf("%w", errJoinedImageIsEmpty)
 	}
@@ -594,11 +614,26 @@ func (tcs *TrailCamSorter) createJoinedImage(inputFile string, frameNumber int, 
 func (tcs *TrailCamSorter) performOCR(imgMat *gocv.Mat) (string, error) {
 	// Create a new Tesseract client.
 	client := gosseract.NewClient()
-	defer client.Close()
+	defer func() {
+		err := client.Close()
+		if err != nil {
+			log.WithFields(log.Fields{
+				"error": err,
+			}).Error("Error closing Tesseract client")
+		}
+	}()
 
 	// Convert the image to grayscale.
 	grayMat := gocv.NewMat()
-	defer grayMat.Close()
+	defer func() {
+		err := grayMat.Close()
+		if err != nil {
+			log.WithFields(log.Fields{
+				"error": err,
+			}).Error("Error closing grayMat")
+		}
+	}()
+
 	gocv.CvtColor(*imgMat, &grayMat, gocv.ColorBGRToGray)
 
 	// Convert the grayscale image to a PNG byte slice.
